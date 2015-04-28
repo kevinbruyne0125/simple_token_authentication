kevinbruyne0125/simple_token_authentication

commit e797557c3d54cc15bf248e04431121164359e242

Refactor extract TokenAuthenticatable
The module specs are largely copied from the specs of
ActsAsTokenAuthenticatable. In order to keep the diffs
readable, I'll clean those in the next two commits.

lib/simple_token_authentication.rb
@@ -11,7 +11,7 @@ module SimpleTokenAuthentication
 
   def self.ensure_models_can_act_as_token_authenticatables model_adapters
     model_adapters.each do |model_adapter|
-      model_adapter.base_class.send :include, SimpleTokenAuthentication::ActsAsTokenAuthenticatable
+      model_adapter.base_class.send :extend, SimpleTokenAuthentication::ActsAsTokenAuthenticatable
     end
   end
 

lib/simple_token_authentication/acts_as_token_authenticatable.rb
@@ -1,49 +1,14 @@
-require 'active_support/concern'
-require 'simple_token_authentication/token_generator'
+require 'simple_token_authentication/token_authenticatable'
 
 module SimpleTokenAuthentication
   module ActsAsTokenAuthenticatable
-    extend ::ActiveSupport::Concern
 
-    # Please see https://gist.github.com/josevalim/fb706b1e933ef01e4fb6
-    # before editing this file, the discussion is very interesting.
+    # This module ensures that no TokenAuthenticatable behaviour
+    # is added before the class actually `acts_as_token_authenticatable`.
 
-    included do
-      private :generate_authentication_token
-      private :token_suitable?
-      private :token_generator
-    end
-
-    # Set an authentication token if missing
-    #
-    # Because it is intended to be used as a filter,
-    # this method is -and should be kept- idempotent.
-    def ensure_authentication_token
-      if authentication_token.blank?
-        self.authentication_token = generate_authentication_token(token_generator)
-      end
-    end
-
-    def generate_authentication_token(token_generator)
-      loop do
-        token = token_generator.generate_token
-        break token if token_suitable?(token)
-      end
-    end
-
-    def token_suitable?(token)
-      self.class.where(authentication_token: token).count == 0
-    end
-
-    # Private: Get one (always the same) object which behaves as a token generator
-    def token_generator
-      @token_generator ||= TokenGenerator.new
-    end
-
-    module ClassMethods
-      def acts_as_token_authenticatable(options = {})
-        before_save :ensure_authentication_token
-      end
+    def acts_as_token_authenticatable(options = {})
+      include SimpleTokenAuthentication::TokenAuthenticatable
+      before_save :ensure_authentication_token
     end
   end
 end

lib/simple_token_authentication/token_authenticatable.rb
@@ -1,0 +1,43 @@
+require 'active_support/concern'
+require 'simple_token_authentication/token_generator'
+
+module SimpleTokenAuthentication
+  module TokenAuthenticatable
+    extend ::ActiveSupport::Concern
+
+    # Please see https://gist.github.com/josevalim/fb706b1e933ef01e4fb6
+    # before editing this file, the discussion is very interesting.
+
+    included do
+      private :generate_authentication_token
+      private :token_suitable?
+      private :token_generator
+    end
+
+    # Set an authentication token if missing
+    #
+    # Because it is intended to be used as a filter,
+    # this method is -and should be kept- idempotent.
+    def ensure_authentication_token
+      if authentication_token.blank?
+        self.authentication_token = generate_authentication_token(token_generator)
+      end
+    end
+
+    def generate_authentication_token(token_generator)
+      loop do
+        token = token_generator.generate_token
+        break token if token_suitable?(token)
+      end
+    end
+
+    def token_suitable?(token)
+      self.class.where(authentication_token: token).count == 0
+    end
+
+    # Private: Get one (always the same) object which behaves as a token generator
+    def token_generator
+      @token_generator ||= TokenGenerator.new
+    end
+  end
+end

spec/lib/simple_token_authentication/token_authenticatable_spec.rb
@@ -1,0 +1,68 @@
+require 'spec_helper'
+
+class DummyTokenGenerator
+  def initialize(args={})
+    @tokens_to_be_generated = args[:tokens_to_be_generated]
+  end
+
+  def generate_token
+    @tokens_to_be_generated.shift
+  end
+end
+
+describe DummyTokenGenerator do
+  it_behaves_like 'a token generator'
+end
+
+describe 'Any instance of a class which includes SimpleTokenAuthentication::TokenAuthenticatable' do
+
+  let(:described_class) do
+    define_dummy_class_which_includes SimpleTokenAuthentication::TokenAuthenticatable
+  end
+
+  after(:each) do
+    # ensure_examples_independence
+    SimpleTokenAuthentication.send(:remove_const, :SomeClass)
+  end
+
+  it_behaves_like 'a token authenticatable'
+
+  let(:subject) { described_class.new() }
+
+  describe '#ensure_authentication_token' do
+
+    context 'when some authentication tokens are already in use' do
+
+      before(:each) do
+        TOKENS_IN_USE = ['ExampleTok3n', '4notherTokeN']
+
+        subject.instance_eval do
+
+          @token_generator = DummyTokenGenerator.new(
+            tokens_to_be_generated: TOKENS_IN_USE + ['Dist1nCt-Tok3N'])
+
+          def authentication_token=(value)
+            @authentication_token = value
+          end
+
+          def authentication_token
+            @authentication_token
+          end
+
+          # the 'ExampleTok3n' is already in use
+          def token_suitable?(token)
+            not TOKENS_IN_USE.include? token
+          end
+        end
+      end
+
+      it 'ensures its authentication token is unique', public: true do
+        subject.ensure_authentication_token
+
+        expect(subject.authentication_token).not_to eq 'ExampleTok3n'
+        expect(subject.authentication_token).not_to eq '4notherTokeN'
+        expect(subject.authentication_token).to eq 'Dist1nCt-Tok3N'
+      end
+    end
+  end
+end

spec/support/spec_for_token_authenticatable_interface.rb
@@ -1,0 +1,8 @@
+RSpec.shared_examples 'a token authenticatable' do
+
+  let(:token_authenticatable) { described_class.new() }
+
+  it 'responds to :ensure_authentication_token', private: true do
+    expect(token_authenticatable).to respond_to :ensure_authentication_token
+  end
+end

commit 180258f279d8f765a59d3d39b91e669ea732afbb

Fix broken MongoidAdapter expectation
Revert "Refactor extract TokenAuthenticatable"
This reverts commit e797557c3d54cc15bf248e04431121164359e242.

The MongoidAdapter is built on the expectation that its `base_class`
will include SimpleTokenAuthentication::ActsAsTokenAuthenticatable
and will automatically provide .acts_as_token_authenticatable to
the classes which will include it.

Example:

    class User
      include Mongoid::Document # the MongoidAdapter.base_class

      # is expected to be able to use
      acts_as_token_athenticatable
    end

That expectation was broken in e797557 because ActsAsTokenAuthenticatable
module did no longer include ActiveSupport::Concern, hence lost its
ability to define class methods when `include`d (vs. `extend`ed).

For the purpose of defining class methods, including two successive
concerns does the job, while including a concern (Mongoid::Document)
that extended ActsAsTokenAuthenticatable (no longer a concern) does not.

Many thanks @krsyoung for pinpointing the cause of the bug.

Fixes #168

lib/simple_token_authentication.rb
@@ -11,7 +11,7 @@ module SimpleTokenAuthentication
 
   def self.ensure_models_can_act_as_token_authenticatables model_adapters
     model_adapters.each do |model_adapter|
-      model_adapter.base_class.send :extend, SimpleTokenAuthentication::ActsAsTokenAuthenticatable
+      model_adapter.base_class.send :include, SimpleTokenAuthentication::ActsAsTokenAuthenticatable
     end
   end
 

lib/simple_token_authentication/acts_as_token_authenticatable.rb
@@ -1,14 +1,49 @@
-require 'simple_token_authentication/token_authenticatable'
+require 'active_support/concern'
+require 'simple_token_authentication/token_generator'
 
 module SimpleTokenAuthentication
   module ActsAsTokenAuthenticatable
+    extend ::ActiveSupport::Concern
 
-    # This module ensures that no TokenAuthenticatable behaviour
-    # is added before the class actually `acts_as_token_authenticatable`.
+    # Please see https://gist.github.com/josevalim/fb706b1e933ef01e4fb6
+    # before editing this file, the discussion is very interesting.
 
-    def acts_as_token_authenticatable(options = {})
-      include SimpleTokenAuthentication::TokenAuthenticatable
-      before_save :ensure_authentication_token
+    included do
+      private :generate_authentication_token
+      private :token_suitable?
+      private :token_generator
+    end
+
+    # Set an authentication token if missing
+    #
+    # Because it is intended to be used as a filter,
+    # this method is -and should be kept- idempotent.
+    def ensure_authentication_token
+      if authentication_token.blank?
+        self.authentication_token = generate_authentication_token(token_generator)
+      end
+    end
+
+    def generate_authentication_token(token_generator)
+      loop do
+        token = token_generator.generate_token
+        break token if token_suitable?(token)
+      end
+    end
+
+    def token_suitable?(token)
+      self.class.where(authentication_token: token).count == 0
+    end
+
+    # Private: Get one (always the same) object which behaves as a token generator
+    def token_generator
+      @token_generator ||= TokenGenerator.new
+    end
+
+    module ClassMethods
+      def acts_as_token_authenticatable(options = {})
+        before_save :ensure_authentication_token
+      end
     end
   end
 end

lib/simple_token_authentication/token_authenticatable.rb
@@ -1,43 +1,0 @@
-require 'active_support/concern'
-require 'simple_token_authentication/token_generator'
-
-module SimpleTokenAuthentication
-  module TokenAuthenticatable
-    extend ::ActiveSupport::Concern
-
-    # Please see https://gist.github.com/josevalim/fb706b1e933ef01e4fb6
-    # before editing this file, the discussion is very interesting.
-
-    included do
-      private :generate_authentication_token
-      private :token_suitable?
-      private :token_generator
-    end
-
-    # Set an authentication token if missing
-    #
-    # Because it is intended to be used as a filter,
-    # this method is -and should be kept- idempotent.
-    def ensure_authentication_token
-      if authentication_token.blank?
-        self.authentication_token = generate_authentication_token(token_generator)
-      end
-    end
-
-    def generate_authentication_token(token_generator)
-      loop do
-        token = token_generator.generate_token
-        break token if token_suitable?(token)
-      end
-    end
-
-    def token_suitable?(token)
-      self.class.where(authentication_token: token).count == 0
-    end
-
-    # Private: Get one (always the same) object which behaves as a token generator
-    def token_generator
-      @token_generator ||= TokenGenerator.new
-    end
-  end
-end

spec/lib/simple_token_authentication/token_authenticatable_spec.rb
@@ -1,68 +1,0 @@
-require 'spec_helper'
-
-class DummyTokenGenerator
-  def initialize(args={})
-    @tokens_to_be_generated = args[:tokens_to_be_generated]
-  end
-
-  def generate_token
-    @tokens_to_be_generated.shift
-  end
-end
-
-describe DummyTokenGenerator do
-  it_behaves_like 'a token generator'
-end
-
-describe 'Any instance of a class which includes SimpleTokenAuthentication::TokenAuthenticatable' do
-
-  let(:described_class) do
-    define_dummy_class_which_includes SimpleTokenAuthentication::TokenAuthenticatable
-  end
-
-  after(:each) do
-    # ensure_examples_independence
-    SimpleTokenAuthentication.send(:remove_const, :SomeClass)
-  end
-
-  it_behaves_like 'a token authenticatable'
-
-  let(:subject) { described_class.new() }
-
-  describe '#ensure_authentication_token' do
-
-    context 'when some authentication tokens are already in use' do
-
-      before(:each) do
-        TOKENS_IN_USE = ['ExampleTok3n', '4notherTokeN']
-
-        subject.instance_eval do
-
-          @token_generator = DummyTokenGenerator.new(
-            tokens_to_be_generated: TOKENS_IN_USE + ['Dist1nCt-Tok3N'])
-
-          def authentication_token=(value)
-            @authentication_token = value
-          end
-
-          def authentication_token
-            @authentication_token
-          end
-
-          # the 'ExampleTok3n' is already in use
-          def token_suitable?(token)
-            not TOKENS_IN_USE.include? token
-          end
-        end
-      end
-
-      it 'ensures its authentication token is unique', public: true do
-        subject.ensure_authentication_token
-
-        expect(subject.authentication_token).not_to eq 'ExampleTok3n'
-        expect(subject.authentication_token).not_to eq '4notherTokeN'
-        expect(subject.authentication_token).to eq 'Dist1nCt-Tok3N'
-      end
-    end
-  end
-end

spec/support/spec_for_token_authenticatable_interface.rb
@@ -1,8 +1,0 @@
-RSpec.shared_examples 'a token authenticatable' do
-
-  let(:token_authenticatable) { described_class.new() }
-
-  it 'responds to :ensure_authentication_token', private: true do
-    expect(token_authenticatable).to respond_to :ensure_authentication_token
-  end
-end